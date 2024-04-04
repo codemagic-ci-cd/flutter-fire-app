0801b8a8f3348c375cba7954ac793f6f2bb543d2
Himesh Panchal <106069365+himesh-cm@users.noreply.github.com>
2024-04-04 14:29:03 +0530

    remove main.dart file

--- flutterfire_app/test/widget_test.dart
+++ flutterfire_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutterfire_app/main.dart';
+import 'package:flutterfire_app/main-stg.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
